5948de8fd48389fbc13aeef6433d955fe2b9e376
sagar <sagar@sagars-MacBook-Air.local>
2025-02-07 18:25:07 +0000

    modified to add docker file

--- Bash/build.sh
+++ Bash/build.sh
@@ -3,8 +3,8 @@ sudo docker rmi -f $(sudo docker images -q) ##this is not recommned step, i am d
 sudo rm -r gold ## these steps are not recommened instead you can modify script as shown below
 sudo mkdir gold
 cd gold/
-sudo git clone https://github.com/Hari0o/Gold_Site_Ecommerce.git
-cd Gold_Site_Ecommerce/
-sudo docker build -t react-nginx -f golddockerfile .
-sudo docker tag react-nginx:latest reactdemo/react-nginx:latest ##make sure you did docker login
-sudo docker push reactdemo/react-nginx:latest
+sudo git clone https://github.com/sagarkakkalasworld/Day4.git
+cd Day4/Gold-site-Ecommerce
+sudo docker build -t react-microk8s -f golddockerfile .
+sudo docker tag react-nginx:latest sagarkakkalasworld/react-microk8s:latest ##make sure you did docker login
+sudo docker push sagarkakkalasworld/react-microk8s:latest
